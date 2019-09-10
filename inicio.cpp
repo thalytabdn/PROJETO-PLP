412297e105dfb7b9ff5f9dc585edc7de08321d3e
Matheus Macedo <soueuounaoquemsabe@gmail.com>
2019-09-10 02:28:34 -0300

    adicionando exibicao de disciplina e mudanca de nota(windows)

--- inicio.cpp
+++ inicio.cpp
@@ -18,7 +18,7 @@ struct nota
 
 struct disciplina
 {
-
+    int qtdNotasCadastradas;
 	vector<nota> notas;
 	string nome;
 	string sala;
@@ -30,6 +30,96 @@ string indicadorVazio = "  ";
 
 vector<disciplina> bancoDados;
 
+double alteraValor(){
+
+    double novoValor;
+    cout << endl;
+    cout << "Por favor digite o novo valor" << endl;
+    cin >> novoValor;
+
+    return novoValor;
+}
+
+int manipularDisciplina(int indice){
+
+    char keyPressed;
+    vector<string> lista;
+    int posIndicador = 0;
+
+    lista.push_back(indicador);
+    for (int i = 1; i < bancoDados[indice].notas.size(); i++){
+
+        lista.push_back(indicadorVazio);
+    }
+
+    string coluna[3];
+    int posX = 0;
+
+
+    while(1){
+        system("CLS");
+
+
+        cout << "Disciplina: " << bancoDados[indice].nome << endl;
+        cout << "Sala: " << bancoDados[indice].sala <<  " | Professor: " << bancoDados[indice].professor << endl;
+        cout << endl;
+        cout << "Relatorio de notas:" << endl;
+        if (bancoDados[indice].qtdNotasCadastradas == 0){
+            cout << "Ainda nao posssui notas cadastradas, por favor cadastre abaixo ^-^" << endl;
+        }
+
+        cout << endl;
+        cout << "Avaliacao - nota - peso:" << endl;
+        cout << endl;
+
+        double media = 0;
+        double pesoTotal = 0;
+        for (int i = 0; i < bancoDados[indice].notas.size(); i++){
+
+            media = media + (bancoDados[indice].notas[i].nota*bancoDados[indice].notas[i].peso);
+            pesoTotal = pesoTotal + bancoDados[indice].notas[i].peso;
+
+            coluna[posX] = lista[i];
+            cout << coluna[0] << bancoDados[indice].notas[i].nomeNota << " - " <<
+            coluna[1] << bancoDados[indice].notas[i].nota << " - " << coluna[2] << bancoDados[indice].notas[i].peso <<endl;
+            coluna[posX] = "";
+        }
+
+        cout << endl;
+        media = media /pesoTotal;
+
+        cout << "Media Acumulada: " << media << endl;
+
+        keyPressed = getch();
+
+        if (int(keyPressed) == int('s'))
+		{
+			lista[posIndicador] = indicadorVazio;
+			posIndicador = (posIndicador + 1) % bancoDados[indice].notas.size();
+			lista[posIndicador] = indicador;
+		}
+
+		if (int(keyPressed) == int('a'))
+		{
+			return 1;
+		}
+
+		if (int(keyPressed) == int('d')){
+
+            posX = (posX + 1) % 3;
+		}
+
+		if (int(keyPressed) == int('g') && posX != 0){
+
+            if (posX == 1){
+                bancoDados[indice].notas[posIndicador].nota = alteraValor();
+            }
+            if (posX == 2){
+                bancoDados[indice].notas[posIndicador].peso = alteraValor();
+            }
+		}
+    }
+}
 
 int exibeDisciplinas()
 {
@@ -79,6 +169,13 @@ int exibeDisciplinas()
 		{
 			return 1;
 		}
+
+		if (int(keyPressed) == int('d')){
+
+            if (manipularDisciplina(posIndicador) == -1){
+                return -1;
+            }
+		}
 	}
 }
 
@@ -116,20 +213,16 @@ int exibeConfiguracoes()
 		{
 			if (posIndicador == 0)
 			{
-				
+
 			return 1;
 			}
 			else{
-				
+
 					return 1;
 				}
-			
+
 		}
 		else if(int(keyPressed) == int('a')){
-			return 1;
-		}
-		
-		else{
 			return 1;
 		}
 
@@ -197,6 +290,15 @@ int main()
 	dis.nome = "ahahah";
 	dis.professor = "ffff";
 	dis.sala = "hell";
+
+	nota n;
+	n.nomeNota = "oal";
+	n.nota = 8.6;
+
+	dis.notas.push_back(n);
+    dis.notas.push_back(n);
+    dis.notas.push_back(n);
+
 	bancoDados.push_back(dis);
 	bancoDados.push_back(dis);
 	run();
