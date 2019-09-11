66f03c5ceac096068d2bed48fec5e63f83433f29
Matheus Macedo <soueuounaoquemsabe@gmail.com>
2019-09-10 21:10:17 -0300

    adicionando analise de notas

--- inicio.cpp
+++ inicio.cpp
@@ -9,21 +9,25 @@ using namespace std;
 
 struct nota
 {
-
 	double peso;
 	double nota;
 	string nomeNota;
+	bool dadoUsavel;
 };
 
 
 struct disciplina
 {
-    int qtdNotasCadastradas;
 	vector<nota> notas;
 	string nome;
 	string sala;
 	string professor;
 };
+
+double mediaAprovacaoPadrao = 7.0;
+double mediaAprovacaoFinal = 5.0;
+double mediaMinimaFinal = 4.0;
+double valorProvaFinal = 0.4;
 
 string indicador = "->";
 string indicadorVazio = "  ";
@@ -40,6 +44,87 @@ double alteraValor(){
     return novoValor;
 }
 
+double getNotaMaximaFinal(double media){
+
+    double valorMedia = (1 - valorProvaFinal);
+
+    return (valorMedia*media) + (valorProvaFinal*10);
+}
+
+double getNotaMinimaFinal(double media){
+
+    double valorMedia = (1 - valorProvaFinal);
+
+    return (mediaAprovacaoFinal - (valorMedia*(media)))/valorProvaFinal;
+}
+
+void exibeRelatorioNotas(int indice, double somaValoresConsiderados, double somaValoresDesconsiderados,  double pesoConsiderado, double pesoDesconsiderado){
+
+    cout << endl;
+    cout << "Relatorio de notas:" << endl;
+    cout << endl;
+
+    int qtdNotasUsaveis = 0;
+    int qtdNotas = bancoDados[indice].notas.size();
+
+    for (int i = 0; i < qtdNotas; i++){
+
+        if (bancoDados[indice].notas[i].dadoUsavel){
+            qtdNotasUsaveis++;
+        }
+    }
+
+    if (qtdNotas == qtdNotasUsaveis){
+
+        double media = somaValoresConsiderados / pesoConsiderado;
+
+        if (media >= mediaAprovacaoPadrao){
+            cout << "Voce ja esta aprovado" << endl;
+        }
+        else{
+            double nFinal = getNotaMinimaFinal(media);
+            if (media < mediaMinimaFinal){
+
+                cout << "Voce ja esta reprovado" << endl;
+            }
+            else{
+                double nFinalMaxima = getNotaMaximaFinal(media);
+                cout << "Voce esta na final precisando de: " << nFinal << " para ser aprovado." << endl;
+                cout << endl;
+                cout << "Podendo ter media maxima de: " << nFinalMaxima << " caso tire 10 na final" << endl;
+            }
+
+        }
+    }
+
+    else{
+        double media = somaValoresConsiderados / (pesoConsiderado + pesoDesconsiderado);
+        double maximoFaltante = ((pesoDesconsiderado *10)/(pesoConsiderado+pesoDesconsiderado));
+
+        if (media >= mediaAprovacaoPadrao){
+            cout << "Parabens voce esta aprovado com media: " << media << ",caso voce tire 0 nas proximas notas" << endl;
+            cout << "Caso voce tire 100% nas proximas provas, sua media sera: " << media + maximoFaltante << endl;
+        }
+        else if ((media + maximoFaltante) >= mediaAprovacaoPadrao){
+            cout << "Voce ainda nao foi aprovado, entretanto voce tem que acumular no minimo: " << mediaAprovacaoPadrao - media << ", para ser aprovado." << endl;
+            cout << "Podendo ter no maximo a media de: " << maximoFaltante + media << endl;
+        }
+
+        else{
+
+            if (media+maximoFaltante < mediaAprovacaoFinal){
+
+                cout << "Voce ja esta reprovado" << endl;
+            }
+            else{
+
+                cout << "Voce ja esta na final" << endl;
+            }
+        }
+
+    }
+}
+
 int manipularDisciplina(int indice){
 
     char keyPressed;
@@ -52,7 +137,7 @@ int manipularDisciplina(int indice){
         lista.push_back(indicadorVazio);
     }
 
-    string coluna[3];
+    string coluna[4];
     int posX = 0;
 
 
@@ -62,33 +147,41 @@ int manipularDisciplina(int indice){
 
         cout << "Disciplina: " << bancoDados[indice].nome << endl;
         cout << "Sala: " << bancoDados[indice].sala <<  " | Professor: " << bancoDados[indice].professor << endl;
-        cout << endl;
-        cout << "Relatorio de notas:" << endl;
-        if (bancoDados[indice].qtdNotasCadastradas == 0){
-            cout << "Ainda nao posssui notas cadastradas, por favor cadastre abaixo ^-^" << endl;
-        }
 
-        cout << endl;
-        cout << "Avaliacao - nota - peso:" << endl;
+        cout << "Avaliacao - nota - peso -|- Usar dado:" << endl;
         cout << endl;
 
-        double media = 0;
-        double pesoTotal = 0;
+        double somaValoresConsiderados = 0;
+        double somaValoresDesconsiderados = 0;
+        double pesoConsiderado = 0;
+        double pesoDesconsiderado = 0;
+
         for (int i = 0; i < bancoDados[indice].notas.size(); i++){
 
-            media = media + (bancoDados[indice].notas[i].nota*bancoDados[indice].notas[i].peso);
-            pesoTotal = pesoTotal + bancoDados[indice].notas[i].peso;
+            if (bancoDados[indice].notas[i].dadoUsavel){
+                somaValoresConsiderados += (bancoDados[indice].notas[i].nota*bancoDados[indice].notas[i].peso);
+                pesoConsiderado += bancoDados[indice].notas[i].peso;
+            }
+            else{
+                somaValoresDesconsiderados += (bancoDados[indice].notas[i].nota*bancoDados[indice].notas[i].peso);
+                pesoDesconsiderado += bancoDados[indice].notas[i].peso;
+            }
+
 
             coluna[posX] = lista[i];
             cout << coluna[0] << bancoDados[indice].notas[i].nomeNota << " - " <<
-            coluna[1] << bancoDados[indice].notas[i].nota << " - " << coluna[2] << bancoDados[indice].notas[i].peso <<endl;
+            coluna[1] << bancoDados[indice].notas[i].nota << " - " <<
+            coluna[2] << bancoDados[indice].notas[i].peso << " % -|- " <<
+            coluna[3] << bancoDados[indice].notas[i].dadoUsavel << endl;
             coluna[posX] = "";
         }
 
         cout << endl;
-        media = media /pesoTotal;
+        double media = (somaValoresConsiderados + somaValoresDesconsiderados) / (pesoConsiderado + pesoDesconsiderado);
 
         cout << "Media Acumulada: " << media << endl;
+
+        exibeRelatorioNotas(indice, somaValoresConsiderados, somaValoresDesconsiderados, pesoConsiderado, pesoDesconsiderado);
 
         keyPressed = getch();
 
@@ -106,7 +199,7 @@ int manipularDisciplina(int indice){
 
 		if (int(keyPressed) == int('d')){
 
-            posX = (posX + 1) % 3;
+            posX = (posX + 1) % 4;
 		}
 
 		if (int(keyPressed) == int('g') && posX != 0){
@@ -116,6 +209,9 @@ int manipularDisciplina(int indice){
             }
             if (posX == 2){
                 bancoDados[indice].notas[posIndicador].peso = alteraValor();
+            }
+            if (posX == 3){
+                bancoDados[indice].notas[posIndicador].dadoUsavel = !bancoDados[indice].notas[posIndicador].dadoUsavel;
             }
 		}
     }
@@ -293,9 +389,12 @@ int main()
 
 	nota n;
 	n.nomeNota = "oal";
-	n.nota = 8.6;
+	n.nota = 7;
+	n.peso = 25;
+	n.dadoUsavel = false;
 
 	dis.notas.push_back(n);
+    dis.notas.push_back(n);
     dis.notas.push_back(n);
     dis.notas.push_back(n);
 
