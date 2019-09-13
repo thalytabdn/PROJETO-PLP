6ee23cc005ccf3b80e5268fcdb1ee9df47a7ff21
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2019-09-12 22:48:38 -0300

    adicionando percistencia de arquivos

--- projetoLinux.cpp
+++ projetoLinux.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 
+// biblioteca para getchar
 #include <termios.h>
 #include <unistd.h>
 #include <stdio.h>
 
+// biblioteca para clear
 #include <stdlib.h>
+
+// biblioteca para armazenamento em arquivos
+#include <fstream>
+
+// bibliotecas gerais
 #include <string>
 #include <vector>
 
 
 using namespace std;
 
-
+// estrutura de representacao para nota
 struct nota
 {
 	double peso;
@@ -21,24 +28,41 @@ struct nota
 };
 
 
+// estrutura de representacao de disciplina
 struct disciplina
 {
-	vector<nota> notas;
 	string nome;
 	string sala;
 	string professor;
+	vector<nota> notas;
 };
 
+// valores padrao para analise de aprovacao|final
+
+// media aprovacao direta
 double mediaAprovacaoPadrao = 7.0;
+
+// media aprovacoa em prova final
 double mediaAprovacaoFinal = 5.0;
+
+// media para se qualificar para final
 double mediaMinimaFinal = 4.0;
+
+// peso da prova final para media da prova final
 double valorProvaFinal = 0.4;
 
+// caracteres relacionados ao cursor
+
+// presenca de cursor
 string indicador = "->";
+
+// ausencia de cursor
 string indicadorVazio = "  ";
 
+// estrutura base de dados de informações relacionadas a disciplinas
 vector<disciplina> bancoDados;
 
+// funcao responsavel por ler o movimento do cursor, janelas e acoes do usuario
 char getch(){
 
     termios velho, novo;
@@ -46,16 +70,116 @@ char getch(){
 
     tcgetattr( STDIN_FILENO, &velho);
     novo = velho;
+
     novo.c_lflag &= ~( ICANON | ECHO);
     tcsetattr( STDIN_FILENO,TCSANOW, &novo);
+
     x = getchar();
+
     tcsetattr( STDIN_FILENO,TCSANOW, &velho);
+
     return x;
 }
 
+// limpa a estrutura de armazenamento de disciplinas
+void limpaBancoDados(){
+
+    ofstream ofs;
+
+    // limpando arquivo base de disciplina
+    ofs.open("BancoDados/disciplinas.txt", std::ofstream::out | std::ofstream::trunc);
+
+    // informando a quantidade de disciplinas cadastradas
+    ofs << 0;
+    ofs.close();
+}
+
+void salvaBancoDados(){
+
+    ofstream ofs;
+
+    ofs.open("BancoDados/disciplinas.txt", ofstream::app);
+
+    // inserindo a quantidade de disciplinas a serem cadastradas
+    ofs << bancoDados.size() << endl;
+
+    // inserindo informacoes no arquivo de forma ordenada
+    for (int i = 0; i < bancoDados.size(); i++){
+
+        // inserindo informacoes basicas de disciplina
+        ofs << bancoDados[i].nome << endl;
+        ofs << bancoDados[i].sala << endl;
+        ofs << bancoDados[i].professor << endl;
+
+        // lendo quantidade de notas a serem cadastradas
+        int tam = bancoDados[i].notas.size();
+
+        // inserindo quantidade de notas a serem cadastradas
+        ofs << tam << endl;
+
+        // inserindo informacoes de notas
+        for (int j = 0; j < tam; j++){
+
+            ofs << bancoDados[i].notas[j].peso << endl;
+            ofs << bancoDados[i].notas[j].nota << endl;
+            ofs << bancoDados[i].notas[j].nomeNota << endl;
+            ofs << bancoDados[i].notas[j].dadoUsavel << endl;
+         }
+    }
+
+    ofs.close();
+}
+
+// carregando dados do arquivo caso exista
+void carregaBancoDados(){
+
+    ifstream ifs ("BancoDados/disciplinas.txt");
+
+    // caso arquivo possa ser acessado
+    if (ifs.good()){
+
+        int n;
+        // lendo numero de disciplinas cadastradas
+        ifs >> n;
+
+        // para cada disciplina arquivada
+        for (int j = 0; j < n; j++){
+
+            disciplina d;
+
+            // lendo informacoes basicas disciplinas
+            ifs >> d.nome;
+            ifs >> d.sala;
+            ifs >> d.professor;
+
+            // lendo quantidade de notas da disciplina
+            int tam;
+            ifs >> tam;
+
+            // para cada nota da disciplina
+            for (int i = 0; i < tam; i++){
+
+                nota n;
+
+                // lendo informacoes basicas de nota
+                ifs >> n.peso;
+                ifs >> n.nota;
+                ifs >> n.nomeNota;
+                ifs >> n.dadoUsavel;
+                d.notas.push_back(n);
+            }
+
+            // inserindo disciplina no vector de disciplinas
+            bancoDados.push_back(d);
+        }
+    }
+}
+
+// funcao de interacao com o usuario para modificar o valor de um dado da estrutura de nota
 double alteraValor(){
 
     double novoValor;
+
     cout << endl;
     cout << "Por favor digite o novo valor" << endl;
     cin >> novoValor;
@@ -63,6 +187,7 @@ double alteraValor(){
     return novoValor;
 }
 
+// retorna o valor maximo que alguem pode ter na media fazendo final dado sua media padrao
 double getNotaMaximaFinal(double media){
 
     double valorMedia = (1 - valorProvaFinal);
@@ -70,6 +195,7 @@ double getNotaMaximaFinal(double media){
     return (valorMedia*media) + (valorProvaFinal*10);
 }
 
+// retorna o valor media que alguem pode ter na media fazendo final dado sua media padrao, sendo aprovado
 double getNotaMinimaFinal(double media){
 
     double valorMedia = (1 - valorProvaFinal);
@@ -77,11 +203,13 @@ double getNotaMinimaFinal(double media){
     return (mediaAprovacaoFinal - (valorMedia*(media)))/valorProvaFinal;
 }
 
+// exibe um relatorio para o usuario de sua situacao academica, dado os valores das notas que o usuario pede para serem considerados
 void exibeRelatorioNotas(int indice, double somaValoresConsiderados, double somaValoresDesconsiderados,  double pesoConsiderado, double pesoDesconsiderado){
 
     cout << endl;
     cout << "Relatorio de notas:" << endl;
     cout << endl;
+
 
     int qtdNotasUsaveis = 0;
     int qtdNotas = bancoDados[indice].notas.size();
@@ -146,6 +274,7 @@ void exibeRelatorioNotas(int indice, double somaValoresConsiderados, double soma
     }
 }
 
+// interage com o usuario, para confirmar a parada de execução do codigo
 int confirmaFechamento(){
 
     system("clear");
@@ -155,7 +284,8 @@ int confirmaFechamento(){
     cout << "Digite (s) para encerrar a execucao" << endl;
     cin >> resposta;
 
-    if (resposta == 's'){
+    // caso o usuario confirme digitando s envia -1 simbolizando o fim da execução
+    if (resposta == 's' || resposta == 'S'){
 
         system("clear");
         cout << "Ate mais";
@@ -165,37 +295,55 @@ int confirmaFechamento(){
     return 1;
 }
 
+// exibe a s informacoes de uma disciplina identificada por seu indice e permite a manipulacao de suas notas
 int manipularDisciplina(int indice){
 
+    // tecla pressionada pelo usuario
     char keyPressed;
+
+    // lista representando os opcoes verticais que o cursor pode acessar, armazena o cursor ou sua ausencia
     vector<string> lista;
+
+    // indica a posicao que o cursor ocupa na lista
     int posIndicador = 0;
 
-    lista.push_back(indicador);
+    // inicializa as opcoes
+    if (bancoDados[indice].notas.size() != 0){
+        lista.push_back(indicador);
+    }
     for (int i = 1; i < bancoDados[indice].notas.size(); i++){
 
         lista.push_back(indicadorVazio);
     }
 
+    // lista representando as opcoes verticais que se pode acessar, indica em qual coluna esta
     string coluna[4];
+
+    // indica a posicao em que a lista oucupa horizontalmente
     int posX = 0;
 
-
+    // enquanto o usuario n desejar a janela sera exibida
     while(1){
         system("clear");
 
-
+        // exibe dados disciplina
         cout << "Disciplina: " << bancoDados[indice].nome << endl;
         cout << "Sala: " << bancoDados[indice].sala <<  " | Professor: " << bancoDados[indice].professor << endl;
 
         cout << "Avaliacao - nota - peso -|- Usar dado:" << endl;
         cout << endl;
 
+        // dados estatisticos
+        // soma doa valores a serem considerados no calculo de situacao
         double somaValoresConsiderados = 0;
+        // soma dos valores desconsiderados
         double somaValoresDesconsiderados = 0;
+        // peso na nota dos valores considerados no calculo de situacao
         double pesoConsiderado = 0;
+        // peso na nota dos valores desconsiderados
         double pesoDesconsiderado = 0;
 
+        // para cada nota exibe sua representacao e acumular dados estatisticos
         for (int i = 0; i < bancoDados[indice].notas.size(); i++){
 
             if (bancoDados[indice].notas[i].dadoUsavel){
@@ -295,12 +443,14 @@ int exibeDisciplinas(char parametro)
 		lista.push_back(indicadorVazio);
 	}
 
-	lista[0] = indicador;
-	posIndicador = 0;
+    if (lista.size() != 0){
+
+        lista[0] = indicador;
+        posIndicador = 0;
+    }
 
 	while(1)
 	{
-
 		system("clear");
 		if (bancoDados.size() == 0)
 		{
@@ -318,7 +468,7 @@ int exibeDisciplinas(char parametro)
 
 		keyPressed = getch();
 
-		if (int(keyPressed) == int('s'))
+		if (int(keyPressed) == int('s') && lista.size() != 0)
 		{
 
 			lista[posIndicador] = indicadorVazio;
@@ -326,7 +476,7 @@ int exibeDisciplinas(char parametro)
 			lista[posIndicador] = indicador;
 		}
 
-		if (int(keyPressed) == int('w'))
+		if (int(keyPressed) == int('w') && lista.size() != 0)
 		{
 
 			lista[posIndicador] = indicadorVazio;
@@ -344,8 +494,8 @@ int exibeDisciplinas(char parametro)
 		{
 			return 1;
 		}
-		
-		if (parametro == 'e'){
+
+		if (parametro == 'e' && lista.size() != 0){
 			if (int(keyPressed) == int('r')){
 
         	    if (excluiDisciplina(posIndicador) == -1){
@@ -353,7 +503,7 @@ int exibeDisciplinas(char parametro)
             	}
 			}
 		}
-		if (parametro == 'n'){
+		if (parametro == 'n' && lista.size() != 0){
 			if (int(keyPressed) == int('d')){
 
         	    if (manipularDisciplina(posIndicador) == -1){
@@ -366,7 +516,7 @@ int exibeDisciplinas(char parametro)
 }
 
 vector<nota> cadastraNota(){
-	vector<nota> notas; 
+	vector<nota> notas;
 	string resposta;
 	cout << "Deseja sistema de notas padrão? (Y/n) ";
 	getline(cin, resposta);
@@ -388,14 +538,14 @@ vector<nota> cadastraNota(){
 		nota novaNota;
 		cout << "Qual o numero de notas: \n";
 		cin >> numNotas;
-		
+
 		for (int i = 0; i < numNotas; i++)
 		{
 			cout << "Qual o nome da Nota " << i+1 << ": " << endl;
-			
+
 			cin.ignore();
 			getline(cin, newNota);
-			
+
 			cout << "Qual o peso da Nota " << i+1 << ": " << endl;
 			cin >> pesoNota;
 			novaNota.nomeNota = newNota;
@@ -413,7 +563,7 @@ void cadastraDisciplina(){
 	string nomeDisciplina;
 	string professor;
 	string sala;
-	
+
 	disciplina newDisciplina;
 
 	cout << "Digite o nome da disciplina: \n";
@@ -423,14 +573,14 @@ void cadastraDisciplina(){
 	cout << "Digite o nome do professor: \n";
 	getline(cin, professor);
 	newDisciplina.professor = professor;
-	
+
 	cout << "Digite o nome da sala: \n";
 	getline(cin, sala);
 	newDisciplina.sala = sala;
 
 	newDisciplina.notas = cadastraNota();
 	bancoDados.push_back(newDisciplina);
-	
+
 
 }
 
@@ -567,27 +717,10 @@ void run()
 
 int main()
 {
-
-	disciplina dis;
-	dis.nome = "ahahah";
-	dis.professor = "ffff";
-	dis.sala = "hell";
-
-	nota n;
-	n.nomeNota = "oal";
-	n.nota = 7;
-	n.peso = 25;
-	n.dadoUsavel = false;
-
-	dis.notas.push_back(n);
-    dis.notas.push_back(n);
-    dis.notas.push_back(n);
-    dis.notas.push_back(n);
-
-	bancoDados.push_back(dis);
-	bancoDados.push_back(dis);
-
+    carregaBancoDados();
 	run();
+	limpaBancoDados();
+	salvaBancoDados();
 	return 1;
 }
 
