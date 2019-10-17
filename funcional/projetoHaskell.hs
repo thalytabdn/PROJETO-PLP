6bd75f4432534790688012505911a1337abadebe
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2019-10-16 23:21:34 -0300

    terminando funcionalidades de disciplina

--- funcional/projetoHaskell.hs
+++ funcional/projetoHaskell.hs
@@ -28,6 +28,27 @@ type Status = String
 data Compromisso = Compromisso Titulo Detalhe Prioridade Status
                                                                    deriving (Show, Read)
 
+mediaAprovacaoPadrao :: Double
+mediaAprovacaoPadrao = 7.0
+
+mediaAprovacaoFinal :: Double
+mediaAprovacaoFinal = 5.0;
+
+mediaMinimaFinal :: Double 
+mediaMinimaFinal = 4.0;
+
+valorProvaFinal :: Double 
+valorProvaFinal = 0.4;
+
+getNotaMaximaFinal :: Double -> Double
+getNotaMaximaFinal media = ((1 - valorProvaFinal)*media) + (valorProvaFinal*10)
+
+getNotaMinimaFinal :: Double -> Double
+getNotaMinimaFinal media = ((mediaAprovacaoFinal - ((1 - valorProvaFinal)*(media)))/valorProvaFinal)
+
+getMaximoFaltante :: Double -> Double -> Double
+getMaximoFaltante pesoConsiderado pesoDesconsiderado = ((pesoDesconsiderado *10)/(pesoConsiderado+pesoDesconsiderado))
+
 
 getKey :: IO [Char]
 getKey = reverse <$> getKey' ""
@@ -188,7 +209,7 @@ showNotaCursor (Nota peso pontos nomeNota considerar) cursory = do
    else  
       putStrLn ("  " ++ nomeNota ++ " -   " ++ show(pontos) ++ " -   " ++ show(peso) ++ " % - ->" ++ show(considerar))
 
-
+----------------------------------------------------------------------------------------------
 changeNotaDisciplina :: Notas -> Integer -> Integer -> String -> Notas
 changeNotaDisciplina [] nota contador novoNome = []
 changeNotaDisciplina ((Nota peso pontos nomeNota considerar):os) nota contador novoNome
@@ -201,13 +222,72 @@ changeNota ((Disciplina a b c notas):os) disciplina nota contador novoNome
    | contador == disciplina = (Disciplina a b c (changeNotaDisciplina notas nota 0 novoNome)) : changeNota os disciplina nota (contador+1) novoNome
    | otherwise = (Disciplina a b c notas) : changeNota os disciplina nota (contador+1) novoNome
 
+
+changeValorNotaDisciplina :: Notas -> Integer -> Integer -> Double -> Notas
+changeValorNotaDisciplina [] nota contador novaNota  = []
+changeValorNotaDisciplina ((Nota peso pontos nomeNota considerar):os) nota contador novaNota
+   | contador == nota = (Nota peso novaNota nomeNota considerar) : changeValorNotaDisciplina os nota (contador+1) novaNota
+   | otherwise = (Nota peso pontos nomeNota considerar) : changeValorNotaDisciplina os nota (contador+1) novaNota
+
+
+changeValorNota :: Disciplinas -> Integer -> Integer -> Integer -> Double -> Disciplinas
+changeValorNota [] disciplina nota contador novaNota = []
+changeValorNota ((Disciplina a b c notas):os) disciplina nota contador novaNota
+   | contador == disciplina = (Disciplina a b c (changeValorNotaDisciplina notas nota 0 novaNota)) : changeValorNota os disciplina nota (contador+1) novaNota
+   | otherwise = (Disciplina a b c notas) : changeValorNota os disciplina nota (contador+1) novaNota
+
+
+
+changePesoNotaDisciplina :: Notas -> Integer -> Integer -> Double -> Notas
+changePesoNotaDisciplina [] nota contador novoPeso  = []
+changePesoNotaDisciplina ((Nota peso pontos nomeNota considerar):os) nota contador novoPeso
+   | contador == nota = (Nota novoPeso pontos nomeNota considerar) : changePesoNotaDisciplina os nota (contador+1) novoPeso
+   | otherwise = (Nota peso pontos nomeNota considerar) : changePesoNotaDisciplina os nota (contador+1) novoPeso
+
+
+changePesoNota :: Disciplinas -> Integer -> Integer -> Integer -> Double -> Disciplinas
+changePesoNota [] disciplina nota contador novoPeso = []
+changePesoNota ((Disciplina a b c notas):os) disciplina nota contador novoPeso
+   | contador == disciplina = (Disciplina a b c (changePesoNotaDisciplina notas nota 0 novoPeso)) : changePesoNota os disciplina nota (contador+1) novoPeso
+   | otherwise = (Disciplina a b c notas) : changePesoNota os disciplina nota (contador+1) novoPeso
+
+changeConsiderarNotaDisciplina :: Notas -> Integer -> Integer -> Notas
+changeConsiderarNotaDisciplina [] nota contador   = []
+changeConsiderarNotaDisciplina ((Nota peso pontos nomeNota considerar):os) nota contador 
+   | contador == nota = (Nota peso pontos nomeNota (not considerar)) : changeConsiderarNotaDisciplina os nota (contador+1) 
+   | otherwise = (Nota peso pontos nomeNota considerar) : changeConsiderarNotaDisciplina os nota (contador+1) 
+
+
+changeConsiderarNota :: Disciplinas -> Integer -> Integer -> Integer -> Disciplinas
+changeConsiderarNota [] disciplina nota contador = []
+changeConsiderarNota ((Disciplina a b c notas):os) disciplina nota contador 
+   | contador == disciplina = (Disciplina a b c (changeConsiderarNotaDisciplina notas nota 0 )) : changeConsiderarNota os disciplina nota (contador+1) 
+   | otherwise = (Disciplina a b c notas) : changeConsiderarNota os disciplina nota (contador+1) 
+---------------------------------------------------
 getNovoNomeNota :: IO String
 getNovoNomeNota = do 
-   putStrLn("digite um novo nome")
+   putStrLn("\nDigite um novo nome")
+   hSetBuffering stdin LineBuffering
    hSetEcho stdin True
    x <- getLine
    return x
 
+getNovoValorNota :: IO Double
+getNovoValorNota = do 
+   putStrLn("\nDigite um novo valor de nota")
+   hSetBuffering stdin LineBuffering
+   hSetEcho stdin True
+   x <- readLn
+   return x
+
+getNovoPesoNota :: IO Double
+getNovoPesoNota = do 
+   putStrLn("\nDigite um novo peso de nota")
+   hSetBuffering stdin LineBuffering
+   hSetEcho stdin True
+   x <- readLn
+   return x
+-------------------------------------------------------------
 doDisciplinaScreen :: Disciplinas -> Compromissos -> Integer -> Integer -> Integer -> [Char] -> IO()
 doDisciplinaScreen disciplinas compromissos disciplina cursorx cursory action 
    | action == "\ESC[B" = disciplinaScreen disciplinas compromissos disciplina ((cursorx+1) `mod` toInteger(length (getNotasDisciplina (disciplinas !! fromInteger(disciplina))))) cursory
@@ -218,7 +298,15 @@ doDisciplinaScreen disciplinas compromissos disciplina cursorx cursory action
    | action == "\ESC[D" && cursory /= 0 = disciplinaScreen disciplinas compromissos disciplina cursorx (cursory-1)
    | action == "a" && cursory == 0 = do
       x <- getNovoNomeNota
-      disciplinaScreen (changeNota disciplinas disciplina cursorx 0 x) compromissos disciplina cursorx cursory 
+      disciplinaScreen (changeNota disciplinas disciplina cursorx 0 x) compromissos disciplina cursorx cursory
+   | action == "a" && cursory == 1 = do
+      x <- getNovoValorNota
+      disciplinaScreen (changeValorNota disciplinas disciplina cursorx 0 x) compromissos disciplina cursorx cursory
+   | action == "a" && cursory == 2 = do
+      x <- getNovoPesoNota
+      disciplinaScreen (changePesoNota disciplinas disciplina cursorx 0 x) compromissos disciplina cursorx cursory
+   | action == "a" && cursory == 3 = 
+      disciplinaScreen (changeConsiderarNota  disciplinas disciplina cursorx 0 ) compromissos disciplina cursorx cursory
    | otherwise = disciplinaScreen disciplinas compromissos disciplina cursorx cursory
                                                             
 showNotaSemCursor :: Nota -> IO()
@@ -244,12 +332,100 @@ showDisciplinaScreen (Disciplina a b c d) cursorx cursory = do
    
    showNotasScreen d cursorx cursory 0 
 
+getPesoConsiderado :: Notas -> Double
+getPesoConsiderado [] = 0
+getPesoConsiderado ((Nota peso pontos nomeNota considerar): os) 
+   | considerar == True = peso + getPesoConsiderado os
+   | otherwise = 0 + getPesoConsiderado os
+   
+getPesoDesconsiderado :: Notas -> Double   
+getPesoDesconsiderado [] = 0
+getPesoDesconsiderado ((Nota peso pontos nomeNota considerar): os) 
+   | considerar == True = 0 + getPesoDesconsiderado os
+   | otherwise = peso + getPesoDesconsiderado os
+
+
+getValorConsiderado :: Notas -> Double
+getValorConsiderado [] = 0
+getValorConsiderado ((Nota peso pontos nomeNota considerar): os) 
+   | considerar == True = (peso*pontos) + getValorConsiderado os
+   | otherwise = 0 + getValorConsiderado os
+   
+getValorDesconsiderado :: Notas -> Double   
+getValorDesconsiderado [] = 0
+getValorDesconsiderado ((Nota peso pontos nomeNota considerar): os) 
+   | considerar == True = 0 + getValorDesconsiderado os
+   | otherwise = (peso*pontos) + getValorDesconsiderado os 
+   
+
+getMediaConsiderada :: Double -> Double -> Double -> Double -> Double
+getMediaConsiderada pesoC pesoD valorC valorD = (valorC) / (pesoD + pesoC)
+
+getMediaGeral ::  Double -> Double -> Double -> Double -> Double
+getMediaGeral pesoC pesoD valorC valorD = (valorC + valorD) / (pesoD + pesoC)
+
+checaPesoTotalValido :: Double -> Double -> Bool
+checaPesoTotalValido pc pd = (abs (100 - (pc + pd))) <= 0.1
+
+relatorioNotasCompleto :: Double -> IO ()
+relatorioNotasCompleto media 
+   | media >= mediaAprovacaoPadrao = do putStrLn ("'Parabens' :D voce esta aprovado, com media: " ++ (show(media)))
+   
+   | media < mediaMinimaFinal = do putStrLn "VISHI!! :( Voce ja esta 'Reprovado'"
+   
+   | otherwise = do
+      let nFinalMaxima = getNotaMaximaFinal(media)
+      let nFinalMinima = getNotaMinimaFinal (media)
+      putStrLn("Voce esta na final precisando de: " ++ (show nFinalMinima) ++ " para ser aprovado.\n")
+      putStrLn("Podendo ter media maxima de: " ++ (show nFinalMaxima) ++ " caso tire 10 na final\n")
+
+relatorioNotasParcial :: Double -> Double -> IO ()
+relatorioNotasParcial maximoFaltante media 
+   | (media >= mediaAprovacaoPadrao) = do
+      putStrLn("'Parabens' :D voce esta aprovado com media: " ++ (show media) ++ ",caso voce tire 0 nas proximas notas\n")
+      putStrLn("Caso voce tire 100% nas proximas provas, sua media sera: " ++ (show(media + maximoFaltante))++ "\n")
+
+   | media < mediaAprovacaoPadrao && ((media + maximoFaltante) >= mediaAprovacaoPadrao) = do
+      putStrLn("Voce ainda nao foi 'aprovado', entretanto voce tem que acumular no minimo: " ++ (show (mediaAprovacaoPadrao - media)) ++ ", para ser aprovado.\n")
+      putStrLn("Podendo ter no maximo a media de: " ++ (show (maximoFaltante + media)) ++ "\n")
+
+   | otherwise = do
+      if (media+maximoFaltante < mediaAprovacaoFinal) then
+         putStrLn("VISHI!! :( Voce ja esta 'Reprovado'\n")    
+      else
+         putStrLn("CUIDADO!! :O Voce ja esta na 'Final'\n")
+
+relatorioNotas :: Double -> Double -> Double -> Double -> Double -> IO ()
+relatorioNotas pesoConsiderado pesoDesconsiderado valorConsiderado valorDesconsiderado media 
+   | pesoDesconsiderado == 0 = relatorioNotasCompleto media
+   | otherwise = relatorioNotasParcial (getMaximoFaltante pesoConsiderado pesoDesconsiderado) media
+
+showRelatorioSituacao :: Disciplina -> IO ()
+showRelatorioSituacao disciplina = do
+
+   let notas = (getNotasDisciplina disciplina)
+   let pesoConsiderado = getPesoConsiderado notas
+   let pesoDesconsiderado = getPesoDesconsiderado notas
+   let valorConsiderado = getValorConsiderado notas
+   let valorDesconsiderado = getValorDesconsiderado notas
+
+   putStrLn ("\nMedia Acumulada: " ++ (show (getMediaGeral pesoConsiderado pesoDesconsiderado valorConsiderado valorDesconsiderado)))
+
+   if (checaPesoTotalValido pesoConsiderado pesoDesconsiderado == True) then do
+      putStrLn ("\nRelatorio de notas:\n")
+      relatorioNotas pesoConsiderado pesoDesconsiderado valorConsiderado valorDesconsiderado (getMediaConsiderada pesoConsiderado pesoDesconsiderado valorConsiderado valorDesconsiderado)
+            
+   else 
+      putStrLn "\nErro - O valor somado de todos os pesos deve corresponder a 100%"
+
    
 disciplinaScreen :: Disciplinas -> Compromissos -> Integer -> Integer -> Integer -> IO ()
 disciplinaScreen disciplinas compromissos disciplina cursorx cursory = do
    
    system "clear"
    showDisciplinaScreen (disciplinas !! fromInteger(disciplina)) cursorx cursory 
+   showRelatorioSituacao (disciplinas !! fromInteger(disciplina))
+  
    hSetBuffering stdin NoBuffering
    hSetEcho stdin False
    action <- getKey
