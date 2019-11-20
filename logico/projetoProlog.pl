0bad348bc83a1dd43e4c565109f242a4b9e750c8
MatheusClaudi <matheus.claudino@ccc.ufcg.edu.br>
2019-11-20 20:55:38 -0300

    disciplina quase pronta, falta relatorio parcial

--- logico/projetoProlog.pl
+++ logico/projetoProlog.pl
@@ -6,6 +6,23 @@ left(97).
 right(100).
 select(113).
 
+mediaAprovacaoPadrao(7.0).
+mediaAprovacaoFinal(5.0).
+mediaMinimaFinal(4.0).
+valorProvaFinal(0.4).
+
+getNotaMaximaFinal(Media, Nota) :-
+    valorProvaFinal(VPF),
+    Nota is ((1 - VPF)*Media) + (VPF*10).
+
+getNotaMinimaFinal(Media, Nota) :-
+    mediaAprovacaoFinal(MAF),
+    valorProvaFinal(VPF),
+    Nota is ((MAF - ((1 - VPF)*(Media)))/VPF).
+
+getMaximoFaltante(PesoConsiderado, PesoDesconsiderado, Maximo) :-
+    Maximo is ((PesoDesconsiderado *10)/(PesoConsiderado+PesoDesconsiderado)).
+
 upAction(Cursor, Limit, NewCursor) :-
     Cursor =:= 0,
     NewCursor is Limit.
@@ -116,6 +133,67 @@ acessoDisciplinasScreen(ListaCompromissos, ListaDisciplinas, Cursor) :-
 /* tela acesso disciplinas --*/
 
 /* -- tela de uma disciplina */
+
+getPesoConsiderado([], 0).
+getPesoConsiderado([[Nome, Peso, Valor,Considerar]|Ns], Retorno) :- 
+    (Considerar =:= 1 -> getPesoConsiderado(Ns, Retorno2), Retorno = Peso + Retorno2; 
+     getPesoConsiderado(Ns, Retorno2), Retorno = 0 + Retorno2).
+
+getPesoDesconsiderado([], 0).
+getPesoDesconsiderado([[Nome, Peso, Valor,Considerar]|Ns], Retorno) :- 
+    (Considerar =:= 0 -> getPesoDesconsiderado(Ns, Retorno2), Retorno = Peso + Retorno2; 
+     getPesoDesconsiderado(Ns, Retorno2), Retorno = 0 + Retorno2).
+
+getValorConsiderado([], 0).
+getValorConsiderado([[Nome, Peso, Valor,Considerar]|Ns], Retorno) :-
+    (Considerar =:= 1 -> getValorConsiderado(Ns, Retorno2), V is Peso*Valor, Retorno = V + Retorno2;
+     getValorConsiderado(Ns, Retorno2), Retorno = 0 + Retorno2).
+
+getValorDesconsiderado([], 0).
+getValorDesconsiderado([[Nome, Peso, Valor,Considerar]|Ns], Retorno) :-
+    (Considerar =:= 0 -> getValorDesconsiderado(Ns, Retorno2), V is Peso*Valor, Retorno = V + Retorno2;
+    getValorDesconsiderado(Ns, Retorno2), Retorno = 0 + Retorno2).
+
+getMediaConsiderada(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaConsiderada) :-
+    MediaConsiderada is (ValorConsiderado)/(PesoConsiderado + PesoDesconsiderado).
+
+getMediaGeral(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaGeral) :-
+    MediaGeral is (ValorConsiderado+ValorDesconsiderado)/(PesoConsiderado + PesoDesconsiderado).
+
+relatorioNotasCompleto(MediaConsiderada) :-
+    mediaAprovacaoPadrao(MAP),
+    mediaMinimaFinal(MMF),
+    (MediaConsiderada >= MAP -> write("'Parabens' :D voce esta aprovado, com media: "), writeln(MediaConsiderada);
+     MediaConsiderada < MMF -> write("VISHI!! :( Voce ja esta 'Reprovado'");
+     getNotaMaximaFinal(MediaConsiderada, NMF), getNotaMinimaFinal(MediaConsiderada, NMIF), write("Voce esta na final precisando de: "), writeln(NMIF), writeln(" para ser aprovado.\n"), write("Podendo ter media maxima de: "), writeln(NMF), writeln("caso tire 10 na final\n")).
+
+relatorioNotasParcial(MaximoFaltante, MediaConsiderada) :-
+    mediaAprovacaoPadrao(MAP),
+    MM is MediaConsiderada + MaximoFaltante,
+    write("MMM").
+
+relatorioNotas(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaConsiderada) :- 
+    (PesoDesconsiderado =:= 0 -> relatorioNotasCompleto(MediaConsiderada);
+     getMaximoFaltante(PesoConsiderado, PesoDesconsiderado, Maximo), relatorioNotasParcial(Maximo, MediaConsiderada)).
+
+checaPesoTotalValido(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado) :-
+    PesoAtual is (100 - (PesoConsiderado+PesoDesconsiderado)),
+    (PesoAtual =< 0.11, PesoAtual >= 0 -> writeln("\nRelatorio de notas:\n"), getMediaConsiderada(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaConsiderada), relatorioNotas(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaConsiderada);
+     writeln("\nErro - O valor somado de todos os pesos deve corresponder a 100%")).
+
+showRelatorioSituacao(Notas) :-
+
+    getPesoConsiderado(Notas, PesoConsiderado),
+    getPesoDesconsiderado(Notas, PesoDesconsiderado),
+    getValorConsiderado(Notas, ValorConsiderado),
+    getValorDesconsiderado(Notas, ValorDesconsiderado),
+    getMediaGeral(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado, MediaGeral),
+
+    write("\nMedia Acumulada: "),
+    writeln(MediaGeral),
+
+    checaPesoTotalValido(PesoConsiderado, PesoDesconsiderado, ValorConsiderado, ValorDesconsiderado).
+
 showDisciplinaHeader(Nome, Sala, Professor) :-
     write("Disciplina: "), writeln(Nome),
     write("Sala: "), write(Sala), write(" | "), write("Professor: "), writeln(Professor),
@@ -181,6 +259,7 @@ disciplinaScreen(ListaCompromissos, ListaDisciplinas, CursorX, CursorY, IndexDis
     shell(clear),
     nth0(IndexDisciplina, ListaDisciplinas, [Nome,Sala,Professor,Notas]), 
     showDisciplinaScreen(Nome, Sala, Professor, Notas, CursorX, CursorY),
+    showRelatorioSituacao(Notas),
     get_single_char(Action),
     doDisciplinasScreen(ListaCompromissos, ListaDisciplinas, CursorX, CursorY, [Nome,Sala,Professor,Notas], Action, IndexDisciplina).
 /* tela de uma disciplina --*/
